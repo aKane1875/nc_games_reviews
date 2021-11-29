c3e442cc7e7afc6e35acd68932824133b44e78b5
Allan Kane <allanhfc@yahoo.co.uk>
2021-11-29 11:21:30 +0000

    connected to database and creating first table

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,13 @@
+const db = require("../connection");
+
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. create tables
+  return db.query(`CREATE TABLE users (
+    username VARCHAR(150) PRIMARY KEY UNIQUE,
+    avatar_url VARCHAR(250),
+    name VARCHAR(150)
+  )`);
   // 2. insert data
 };
 
