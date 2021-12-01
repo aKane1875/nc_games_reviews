8266ee33f3b82724b968a08b08f839f692b9a6b5
Allan Kane <allanhfc@yahoo.co.uk>
2021-12-01 16:27:47 +0000

    added log for errors if they reach custom 500 error handling

--- errors/errors.js
+++ errors/errors.js
@@ -11,5 +11,6 @@ exports.handleCustomErrors = (err, req, res, next) => {
 };
 
 exports.handle500Errors = (err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ msg: "CUSTOM INTERNAL SERVER ERROR MESSAGE" });
 };
