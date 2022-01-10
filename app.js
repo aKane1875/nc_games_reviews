2f3e9a77d3ecaf0155f02ba791f76a29ac2ec751
Allan Kane <allanhfc@yahoo.co.uk>
2022-01-10 12:22:17 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { handleCustomErrors, handle500Errors } = require("./errors/errors");
 const apiRouter = require("./routers/api.router");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
